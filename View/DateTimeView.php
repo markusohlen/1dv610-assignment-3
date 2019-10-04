6fd3c39516387e29fe98f705efb5a37412985915
markusohlen <mo223dg@student.lnu.se>
2019-10-04 13:22:35 +0200

    Adding a basic layout

---
+++ View/DateTimeView.php
@@ -0,0 +1,19 @@
+<?php
+
+class DateTimeView {
+
+
+	public function show() {
+		date_default_timezone_set("Europe/Stockholm");
+
+		$dayInWeek = date("l");
+		$day = date("jS");
+		$month = date("F");
+		$year = date("o");
+		$currentTime = date("H:i:s");
+		
+		$timeString = "$dayInWeek, the $day of $month $year, The time is $currentTime";
+
+		return '<p>' . $timeString . '</p>';
+	}
+}
