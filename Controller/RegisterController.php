e8be992ce3cde8b1db22a5ded420f19e5ba9e363
markusohlen <mo223dg@student.lnu.se>
2019-10-12 13:36:45 +0200

    Checks credentials when trying to register a new user

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -7,6 +7,10 @@ class RegisterController {
     private $model;
     private $dbModel;
 
+    private $username;
+    private $password;
+    private $passwordRepeat;
+
     public function __construct($rv, $rm, $dbm) {
         $this->view = $rv;
         $this->model = $rm;
@@ -14,6 +18,50 @@ class RegisterController {
     }
 
     public function register() {
-        echo "HELLO FROM REGISTERCONTROLLER";
+        if ($this->view->userPressedRegister() === false) {
+            return;
+        }
+
+        // if ($this->view->userFilledInCredentials() === false) {
+        //     // $this->view->setMissingCredentialsMessage();
+        //     return;
+        // }
+
+        $this->setCredentials();
+
+        if ($this->hasInvalidCredentials() === true) {
+            return;
+        }
+    }
+
+    private function hasInvalidCredentials() {
+        $hasInvalidCreddentials = false;
+        if ($this->model->usernameIsValid($this->username) === false) {
+            $this->view->setInvalidUsernameMessage();
+            $hasInvalidCreddentials = true;
+        }
+
+        if ($this->model->passwordsIsTooShort($this->password, $this->passwordRepeat)) {
+            $this->view->setPasswordTooShortMessage();
+            $hasInvalidCreddentials = true;
+        }
+
+        if ($this->dbModel->userExists($this->username)) {
+            $this->view->setUsernameExistsMessage();
+            $hasInvalidCreddentials = true;
+        }
+
+        if ($this->model->passwordsMatch($this->password, $this->passwordRepeat) === false) {
+            $this->view->setPasswordsDoNotMatchMessage();
+            $hasInvalidCreddentials = true;
+        }
+
+        return $hasInvalidCreddentials;
+    }
+
+    private function setCredentials() {
+        $this->username = $this->view->getRequestUsername();
+        $this->password = $this->view->getRequestPassword();
+        $this->passwordRepeat = $this->view->getRequestPasswordRepeat();
     }
 }
--- View/RegisterView.php
+++ View/RegisterView.php
@@ -3,8 +3,8 @@
 namespace view;
 
 class RegisterView {
-
-    private static $userName = "RegisterView::UserName";
+    private static $register = "RegisterView::Register";
+    private static $username = "RegisterView::UserName";
     private static $registerViewMessage = "RegisterView::Message";
     private static $password = "RegisterView::Password";
 	private static $passwordRepeat = "RegisterView::PasswordRepeat";
@@ -35,18 +35,15 @@ class RegisterView {
 	* @return  void, BUT writes to standard output!
 	*/
 	private function generateRegisterFormHTML($message) {
-		$currentUsername = "";
-		if ($this->personPressedRegister() === true) {
-			$currentUsername = $this->getRequestUserName();
+        $currentUsername = "";
+        $currentPassword = "";
+        $currentPasswordRepeat = "";
+		if ($this->userPressedRegister() === true) {
+            $currentUsername = $this->getRequestUsername();
+            $currentPassword = $this->getRequestPassword();
+            $currentPasswordRepeat = $this->getRequestPasswordRepeat();
 		}
-		$currentPassword = "";
-		if ($this->personPressedRegister() === true) {
-			$currentPassword = $this->getRequestUserName();
-		}
-		$currentPasswordRepeat = "";
-		if ($this->personPressedRegister() === true) {
-			$currentPasswordRepeat = $this->getRequestPasswordRepeat();
-		}
+
 		return '
         <a href="?">Back to Login</a><div class="container" >
           
@@ -55,8 +52,8 @@ class RegisterView {
                     <fieldset>
                     <legend>Register a new user - Write username and password</legend>
                         <p id="' . self::$registerViewMessage . '">' . $this->message .'</p>
-                        <label for="' . self::$userName . '" >Username :</label>
-                        <input type="text" size="20" name="' . self::$userName . '" id="' . self::$userName . '" value="' . $currentUsername . '" />
+                        <label for="' . self::$username . '" >Username :</label>
+                        <input type="text" size="20" name="' . self::$username . '" id="' . self::$username . '" value="' . $currentUsername . '" />
                         <br/>
                         <label for="' . self::$password . '" >Password  :</label>
                         <input type="password" size="20" name="' . self::$password . '" id="' . self::$password . '" value="' . $currentPassword . '" />
@@ -64,14 +61,74 @@ class RegisterView {
                         <label for="' . self::$passwordRepeat . '" >Repeat password  :</label>
                         <input type="password" size="20" name="' . self::$passwordRepeat . '" id="' . self::$passwordRepeat . '" value="' . $currentPasswordRepeat . '" />
                         <br/>
-                        <input id="submit" type="submit" name="RegisterView::Register"  value="Register" />
+                        <input id="submit" type="submit" name="' . self::$register . '"  value="Register" />
                         <br/>
                     </fieldset>
                 </form>
 		';
     }
     
-    private function personPressedRegister() {
+    public function userPressedRegister() {
+        if (isset($_POST[self::$register]) && $_POST[self::$register] === "Register") {
+			return true;
+		}
         return false;
+    }
+
+    public function getRequestUsername() {
+        return $_POST[self::$username];
+    }
+
+    public function getRequestPassword() {
+        return $_POST[self::$password];
+    }
+
+    public function getRequestPasswordRepeat() {
+        return $_POST[self::$passwordRepeat];
+    }
+
+    private function userFilledInUsername() : bool {
+		if (isset($_POST[self::$username]) && empty($_POST[self::$username]) === false) {
+			return true;
+		}
+		return false;
+	}
+
+	private function userFilledInPassword() : bool {
+		if (isset($_POST[self::$password]) && empty($_POST[self::$password]) === false) {
+			return true;
+		}
+		return false;
+    }
+
+    private function userFilledInPasswordRepeat() : bool {
+		if (isset($_POST[self::$passwordRepeat]) && empty($_POST[self::$passwordRepeat]) === false) {
+			return true;
+		}
+		return false;
+    }
+    
+    public function userFilledInCredentials() : bool {
+		return $this->userFilledInUsername() && $this->userFilledInPassword() && $this->userFilledInPasswordRepeat();
+    }
+
+    public function setMissingCredentialsMessage() {
+        $this->message .= "Username has too few characters, at least 3 characters.<br>";
+    }
+
+    public function setInvalidUsernameMessage() {
+        $this->message .= "Username has too few characters, at least 3 characters.<br>";
+    }
+
+    public function setPasswordTooShortMessage() {
+        $this->message .= "Password has too few characters, at least 6 characters.<br>";
+    }
+
+    public function setUsernameExistsMessage() {
+        $this->message .= "User exists, pick another username.<br>";
+    }
+
+    public function setPasswordsDoNotMatchMessage() {
+        $this->message .= "Passwords do not match.";
     }
 }
