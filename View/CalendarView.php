359f6977b86fc2dff4d0d94aa4df50004c78dc64
markusohlen <mo223dg@student.lnu.se>
2019-10-17 19:51:46 +0200

    Creating a calendar with days

--- View/CalendarView.php
+++ View/CalendarView.php
@@ -19,30 +19,41 @@ class CalendarView
 	public function response() : string {
         $response = $this->generateCalendarHTML();
 
-        $response .= $this->generateLogoutButtonHTML();
+        // $response .= $this->generateLogoutButtonHTML();
 
 		return $response;
 	}
 
 	/**
 	* Generate HTML code on the output buffer for the logout button
-	* @return string BUT writes to standard output!
-	*/
-	private function generateLogoutButtonHTML() : string {
-		return '
-			
-		';
-	}
-	
-	/**
-	* Generate HTML code on the output buffer for the logout button
 	* @return , BUT writes to standard output!
 	*/
 	private function generateCalendarHTML() : string {
-		$currentUsername = "";
+		$days = $this->generateDays();
+		return "
+        <h1>Calendar</h1>
+        <form method='get'>
+            <input type='month' name='' id=''>
+        </form>
+        
+        $days
+		";
+    }
+    
+    private function generateDays(): string
+    {
+        $m = "";
 
-		return '
-		<h1>Calendar</h1>
-		';
-	}
+        $month = date("m");
+        $year = date("Y");
+        
+        $days = cal_days_in_month(CAL_GREGORIAN, (int)$month, (int)$year);
+
+        for ($i = 0; $i < $days; $i++)
+        {
+            $d = $i + 1;
+            $m .= "<div style='height: 100px; width: 150px; background-color: #f0f8ff; margin: 5px; float: left;'>$d</div>";
+        }
+        return $m;
+    }
 }
