c33c6f93212121a1f6c7c2fdeb57ea4d632e12c6
markusohlen <mo223dg@student.lnu.se>
2019-10-20 23:01:46 +0200

    Broke out some code in LoginController and made it possible to throw multiple exceptions

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -9,14 +9,19 @@ class LoginController
     private $dbModel;
     private $session;
 
-    public function __construct($lv, $dbm, $sm) {
+    private $hasException = false;
+    private $user;
+
+    public function __construct($lv, $dbm, $sm) 
+    {
         $this->view = $lv;
         $this->model = new \model\LoginModel();
         $this->dbModel = $dbm;
         $this->session = $sm;
     }
 
-    public function start() : void {
+    public function start() : void 
+    {
         if ($this->view->userPressedLogout() === true && $this->session->getIsLoggedIn() === true) 
         {
             $this->doLogout();
@@ -28,33 +33,63 @@ class LoginController
             return;
         }
 
+        $this->login();
+        
+    }
+
+    private function login()
+    {
+        $this->user = $this->view->getUserCredentials();
+
+        $this->checkMissingCredentials();
+        $this->checkInvalidCredentials();
+
+        if ($this->hasException === false)
+        {
+            $this->doLogin();
+        }
+    }
+
+    private function checkMissingCredentials()
+    {
         try 
         {
-            $user = $this->view->getUserCredentials();
-            $this->model->checkMissingCredentials($user->getUsername(), $user->getPassword());
-
-            if ($this->dbModel->userExists($user->getUsername()) === false)
-            {
-                throw new \model\InvalidCredentialsException();
-            }
-
-            if ($this->dbModel->passwordMatch($user->getUsername(), $user->getPassword()) === false)
-            {
-                throw new \model\InvalidCredentialsException();
-            }
-
-            $this->doLogin();
-        } 
-        catch (\model\MissingUsernameException $e) 
+            $this->model->checkMissingUsername($this->user->getUsername());
+        }
+        catch (\model\MissingUsernameException $e)  
         {
+            $this->hasException = true;
             $this->view->setMissingUsernameMessage();
         }
+
+        try 
+        {
+            $this->model->checkMissingPassword($this->user->getPassword());
+        } 
         catch (\model\MissingPasswordException $e)
         {
+            $this->hasException = true;
             $this->view->setMissingPasswordMessage();
+        }
+    }
+
+    private function checkInvalidCredentials()
+    {
+        try 
+        {
+            if ($this->dbModel->userExists($this->user->getUsername()) === false)
+            {
+                throw new \model\InvalidCredentialsException();
+            }
+
+            if ($this->dbModel->passwordMatch($this->user->getUsername(), $this->user->getPassword()) === false)
+            {
+                throw new \model\InvalidCredentialsException();
+            }
         }
         catch (\model\InvalidCredentialsException $e)
         {
+            $this->hasException = true;
             $this->view->setIncorrectCredentialsMessage();
         }
     }
--- Model/LoginModel.php
+++ Model/LoginModel.php
@@ -4,13 +4,16 @@ namespace model;
 
 class LoginModel
 {
-    public function checkMissingCredentials(string $username, string $password)
+    public function checkMissingUsername(string $username)
     {
         if (strlen($username) === 0) // Empty
         {
             throw new MissingUsernameException();
         }
+    }
 
+    public function checkMissingPassword(string $password)
+    {
         if (strlen($password) === 0) // Empty
         {
             throw new MissingPasswordException();
